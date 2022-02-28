f5479128bcb0175e5a01b9faa663f38902da785f
Makoto Konishi <mkonishi@cuon.co.jp>
2022-02-28 10:09:44 +0900

    カスタムバリデーションの追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,6 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  validate { |book| book.errors[:name] << "I don't like exercise" if book.name.include?('exercise') }
 end
