b6f19742b3f8e8b4b14a6b088ea6d5c81e31a61b
Makotox0207 <xiaoxishicheng@gmail.com>
2022-05-22 15:18:03 +0900

    webpackのエラーの修正, seedファイルにデータを作成するコードの追加, books/showの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Publisher.create(
+  name: 'テストユーザー',
+  address: '東京都北区赤羽1-1-1'
+)
+
+(1..20).each do |i|
+  Book.create(
+    name: "Book #{i}",
+    published_on: Date.today.days_ago(i),
+    price: "#{i}000".to_i,
+    sales_status: :now_on_sale,
+    publisher_id: 1
+  )
+end
